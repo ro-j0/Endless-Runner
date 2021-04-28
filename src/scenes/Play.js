2305c1bc95f9d206c959bdc86c11d89a3cac0ce6
Stryker Buffington <sbuffing@ucsc.edu>
2021-04-27 17:08:52 -0700

    added difficulty increments

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -12,8 +12,8 @@ class Play extends Phaser.Scene {
 
     create() {
         // Create ground
-        //this.ground = this.add.rectangle(320, 480, 640, 100, 0x00FF00);
         this.ground = this.add.rectangle(320, 480, 840, 100, 0x00FF00);
+
         // Create Player
         this.player = new Player(this, 100, game.config.height - this.ground.height, 'player').setOrigin(0.5,0);
 
@@ -37,14 +37,30 @@ class Play extends Phaser.Scene {
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
+        // Start on Easy Mode
         this.difficulty = 0;
-        this.score = 0;
         
+        // Speed of obstacles
+        this.currentSpeed = 150;
+
         // Time between obstacle spawns (ms)
         this.SPAWN_RATE = 1000;
+        
+        // Keeps track of time alive
+        this.score = 0;
+        
+        // EASY (0:00-0:30): speed = 150, rate = 1000
+
+        // MEDIUM (0:30-0:59): speed: 200, rate = 800
+        this.MEDIUM_THRESHOLD = 30000;
+
+        // HARD (1:00-INF): speed = 250, rate = 600
+        this.HARD_THRESHOLD = 60000;
+        
+        
 
         // Create the first obstacle
-        let obstacle = new Obstacle(this, 660, this.ground.y - this.ground.height, 'obstacle', 0, 150, 0);
+        let obstacle = new Obstacle(this, 660, this.ground.y - this.ground.height, 'obstacle', 0, this.currentSpeed, 0);
         this.obstacles.push(obstacle);
         this.physics.add.collider(this.player, obstacle);
 
@@ -65,11 +81,15 @@ class Play extends Phaser.Scene {
     }
 
     update(speed, delta) {
+        
         // Update the player
         this.player.update();
 
         // Increase score by time since last frame
         this.increaseScore(delta);
+
+        // Check if we're ready to increase difficulty
+        this.increaseDifficulty();
 
         // Increase Timer variable by time since last frame
         this.spawnTimer += delta;
@@ -94,7 +114,7 @@ class Play extends Phaser.Scene {
     }
 
     spawnObstacle(){
-        let obstacle = new Obstacle(this, 660, this.ground.y - this.ground.height, 'obstacle', 0, 150, this.obstacles[this.obstacles.length-1].scaleY);
+        let obstacle = new Obstacle(this, 660, this.ground.y - this.ground.height, 'obstacle', 0, this.currentSpeed, this.obstacles[this.obstacles.length-1].scaleY);
         this.obstacles.push(obstacle);
         this.physics.add.collider(this.player, obstacle);
     }
@@ -102,6 +122,30 @@ class Play extends Phaser.Scene {
     increaseScore(delta){
         this.score += delta;
         this.scoreLeft.text = Math.round(this.score/1000) + " seconds"; 
+    }
+
+    increaseDifficulty(){
+        // Bounce back quickly if difficulty is maxed
+        if (this.difficulty == 2){
+            return;
+        }
+        if (this.score >= this.HARD_THRESHOLD && this.difficulty < 2){
+            this.difficulty = 2;
+            this.currentSpeed = 250;
+            this.SPAWN_RATE = 600;
+            // let idx;
+            // for (idx = 0; idx < this.obstacles.length; idx++){
+            //     this.obstacles[idx].body.velocity.x = -this.currentSpeed;
+            // }
+        } else if (this.score >= this.MEDIUM_THRESHOLD && this.difficulty < 1){
+            this.difficulty = 1;
+            this.currentSpeed = 200;
+            this.SPAWN_RATE = 800;
+            //let idx;
+            // for (idx = 0; idx < this.obstacles.length; idx++){
+            //     this.obstacles[idx].body.velocity.x = -this.currentSpeed;
+            // }
+        }
     }
 
     gameOver(){
