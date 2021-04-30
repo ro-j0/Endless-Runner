ca348660d577193f8c770e73a5f2ca270c69fbcc
Stryker Buffington <sbuffing@ucsc.edu>
2021-04-29 20:25:05 -0700

    Modified auto jump mechanic

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -11,6 +11,7 @@ class Player extends Phaser.GameObjects.Sprite {
 
         
         this.moveSpeed = 200;
+        this.jumpHeight = 300;
         this.JUMP_RATE = 1500;
         this.jumpTime = this.JUMP_RATE/2;
         
@@ -19,11 +20,18 @@ class Player extends Phaser.GameObjects.Sprite {
 
         // Flag so that the animation plays once per jump
         this.hasSquished = false;
+
+        this.jumping = false;
     }
     
     update(delta) {
         // Player movement
 
+        if (!this.jumping && this.body.touching.down){
+            this.jumping = true;
+            this.Jump();
+        }
+            
         if(keyLEFT.isDown && this.x >= 0) {
             this.body.velocity.x = -this.moveSpeed;
         } else if (keyRIGHT.isDown && this.x <= game.config.width - this.width) {
@@ -32,21 +40,33 @@ class Player extends Phaser.GameObjects.Sprite {
             this.body.velocity.x = 0;
         }
 
-        if (this.jumpTime <= 0)
-        {
-            this.Jump();
-            this.hasSquished = false;       // Reset animation flag
-            this.jumpTime = this.JUMP_RATE; // Reset jump timer
-        }
-        if (this.jumpTime <= this.SQUISH_DURATION && !this.hasSquished){
-            this.indicateJump();
-        }
+        
 
-        this.jumpTime -= delta;
+        // if (this.jumpTime <= 0)
+        // {
+        //     this.Jump();
+        //     this.hasSquished = false;       // Reset animation flag
+        //     this.jumpTime = this.JUMP_RATE; // Reset jump timer
+        // }
+        // if (this.jumpTime <= this.SQUISH_DURATION && !this.hasSquished){
+        //     this.indicateJump();
+        // }
+
+        // this.jumpTime -= delta;
     }
 
+    
+
     Jump(){
-        this.body.setVelocityY(-300);   // jump up
+        //this.jumping = true;
+        console.log("jump called.");
+        this.indicateJump();
+        this.clock = this.scene.time.delayedCall(this.SQUISH_DURATION-1, () => {
+            this.body.setVelocityY(-this.jumpHeight);   // jump up
+            this.jumping = false;
+        }, null, this);
+        
+        
     }
 
     setJumpRate(value){
@@ -54,7 +74,7 @@ class Player extends Phaser.GameObjects.Sprite {
     }
 
     indicateJump(){
-        this.hasSquished = true;
+        //this.hasSquished = true;
         this.scene.tweens.add({
             targets     : this,
             scaleY: 0.7,
